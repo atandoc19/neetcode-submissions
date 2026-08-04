8bd18c46c6a8d01776653fd50c0d305566bd97d9
atandoc19 <atandoc@umich.edu>
2026-08-04 13:33:40 -0400

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int l1 = 0;
+        int l2 = 0;
+        while(l2 < t.size()){
+            if(s[l1] == t[l2]) l1++;
+            l2++;
+        }
+        return (l1 == s.size());
+    }
+};
